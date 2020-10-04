76bbfecd49f41927fc152729bdeca9db7159d75b
shalikaashan@outlook.com <shalikaashan@outlook.com>
2020-10-04 12:37:48 +0530

    error page and loading page

--- src/views/Repositories/Repositories.js
+++ src/views/Repositories/Repositories.js
@@ -3,45 +3,19 @@ import {useDebounce} from "use-debounce";
 import {useQuery} from "@apollo/react-hooks";
 import {FETCH_REPOSITORIES} from "./queries"
 import "./Repositories.scss";
-
+import Loader from "../../components/loader/loader";
+import ErrorPage from "../../components/error_page/error_page";
 
 const Repositories = ({token}) =>{
 	const {data, loading, error} = useQuery(FETCH_REPOSITORIES);
 	console.log(token)
 	if(loading)
-		return <div>Loading</div>;
+		return <Loader/>;
 	else if(error)
-		return <div>{error}</div>;
+		return <ErrorPage error={error}/>;
 	else {
 		console.log(data)
 		return <div>Repositories</div>
 	}
 }
 export default Repositories;
-// export class Repositories extends Component {
-// 	state = {data: null,
-// 		loading: true,
-// 		error: null}
-// 	constructor(props) {
-// 		super(props);
-//
-// 		this.state = {
-// 			data: null,
-// 			loading: true,
-// 			error: null
-// 		}
-// 	}
-// 	componentDidMount() {
-// 		 const {data, loading, error} = useQuery(FETCH_REPOSITORIES);
-// 	}
-//
-// 	render() {
-// 		if(this.props.loading)
-// 			return <div>Loading</div>;
-// 		else if(this.props.error)
-// 			return <div>error</div>;
-// 		else return <div>Repositories</div>;
-// 	}
-// }
-//
-// export default Repositories;
