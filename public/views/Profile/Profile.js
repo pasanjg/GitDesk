b4bcfb3a59a0581d6926dcb2ac0d068a30e49e0c
pasanjg <44201430+pasanjg@users.noreply.github.com>
2021-05-16 06:56:04 +0530

    Profile UI updated

--- public/views/Profile/Profile.js
+++ public/views/Profile/Profile.js
@@ -21,27 +21,54 @@ export default function Profile() {
 
 		return (
 			<div className="profile-content ">
-				<div className="row m-0">
-					<div className="col-12 col-md-8 user-details">
-						<div>
-							<h1 className="name mt-2">{data.viewer.name}</h1>
-							<h4>@{data.viewer.login}</h4>
+				<div className="container">
+					<div className="row m-0">
+						<div className="col-12 text-center">
+							<img className="avatar-img" src={data.viewer.avatarUrl} alt="avatar" />
 						</div>
-						<div>
-							<p className="mt-1">{data.viewer.bio}</p>
+						<div className="col-12 text-center">
+							<h1 className="name mt-4">{data.viewer.name}</h1>
+							<h4>@{data.viewer.login}</h4>
+							<p className="mt-2 text-muted">{data.viewer.bio}</p>
+						</div>
+						<div className="col-12 user-details mt-4">
 							<div className="stats">
-								{data.viewer.location ? <span><i className="fa fa-map-marker-alt"></i> &nbsp; {data.viewer.location}</span> : <i></i>}
-								{data.viewer.company ? <span><i className="fa fa-building"></i> &nbsp; {data.viewer.company}</span> : <i></i>}
+								<span className="font-weight-bold">{data.viewer.followers.totalCount}</span>
+								<span>Followers</span>
+							</div>
+							<div className="stats">
+								<span className="font-weight-bold">{data.viewer.following.totalCount}</span>
+								<span>Following</span>
+							</div>
+							<div className="stats">
+								<span className="font-weight-bold">{data.viewer.starredRepositories.totalCount}</span>
+								<span>Starred</span>
+							</div>
+							<div className="stats">
+								<span>
+									{
+										data.viewer.location ?
+											<span>
+												<svg class="github-icon octicon octicon-location" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+													<path className="gh-icon" fill-rule="evenodd" d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"></path>
+												</svg>
+												&nbsp; {data.viewer.location}
+											</span> : <i></i>
+									}
+								</span>
+								<span>
+									{
+										data.viewer.company ?
+											<span>
+												<svg class="github-icon octicon octicon-organization" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+													<path className="gh-icon" fill-rule="evenodd" d="M1.5 14.25c0 .138.112.25.25.25H4v-1.25a.75.75 0 01.75-.75h2.5a.75.75 0 01.75.75v1.25h2.25a.25.25 0 00.25-.25V1.75a.25.25 0 00-.25-.25h-8.5a.25.25 0 00-.25.25v12.5zM1.75 16A1.75 1.75 0 010 14.25V1.75C0 .784.784 0 1.75 0h8.5C11.216 0 12 .784 12 1.75v12.5c0 .085-.006.168-.018.25h2.268a.25.25 0 00.25-.25V8.285a.25.25 0 00-.111-.208l-1.055-.703a.75.75 0 11.832-1.248l1.055.703c.487.325.779.871.779 1.456v5.965A1.75 1.75 0 0114.25 16h-3.5a.75.75 0 01-.197-.026c-.099.017-.2.026-.303.026h-3a.75.75 0 01-.75-.75V14h-1v1.25a.75.75 0 01-.75.75h-3zM3 3.75A.75.75 0 013.75 3h.5a.75.75 0 010 1.5h-.5A.75.75 0 013 3.75zM3.75 6a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5zM3 9.75A.75.75 0 013.75 9h.5a.75.75 0 010 1.5h-.5A.75.75 0 013 9.75zM7.75 9a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5zM7 6.75A.75.75 0 017.75 6h.5a.75.75 0 010 1.5h-.5A.75.75 0 017 6.75zM7.75 3a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5z"></path>
+												</svg>
+												&nbsp; {data.viewer.company}
+											</span> : <i></i>
+									}
+								</span>
 							</div>
 						</div>
-						<div className="stats">
-							<span>{data.viewer.followers.totalCount} Followers</span>
-							<span>{data.viewer.following.totalCount} Following</span>
-							<span><i className="fa fa-star"></i> {data.viewer.starredRepositories.totalCount} Starred Repositories</span>
-						</div>
-					</div>
-					<div className="col-12 col-md-4">
-						<img className="avatar-img" src={data.viewer.avatarUrl} alt="avatar" />
 					</div>
 				</div>
 				<div className="row m-0">
@@ -54,22 +81,24 @@ export default function Profile() {
 										<div key={index} className="col-md-6">
 											<div className="repo-card">
 												<div className="details">
-													<h5>{repo.name}</h5>
+													<h5 className="title">{repo.name}</h5>
 													{repo.isPrivate ? <i className="fa fa-lock"></i> : <i></i>}
 												</div>
-												<h6 className="text-muted">{repo.nameWithOwner}</h6>
+												<h6 className="text-muted text-owner">{repo.nameWithOwner}</h6>
 												<div className="languages">
 													{
-														repo.languages.nodes.length > 0 ? 
+														repo.languages.nodes.length > 0 ?
 															repo.languages.nodes.map((language, index) => {
 																return (
 																	<span key={index} style={{ backgroundColor: `${language.color}` }}>{language.name}</span>
 																);
-															}) : 
+															}) :
 															<span key={index} style={{ backgroundColor: "#313131" }}>N/A</span>
 													}
 												</div>
-												{repo.description ? <p className="mb-1">{repo.description}</p> : <p className="text-secondary mb-1">No description</p>}
+												<div className="description">
+													{repo.description ? <p className="mb-1">{repo.description}</p> : <p className="text-secondary mb-1">No description</p>}
+												</div>
 												<div className="meta">
 													<div className="collaborators">
 														<span className="text-muted">Built by </span>
